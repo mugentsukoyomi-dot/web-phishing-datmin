7bb3ba82561284e34546a220eea357201270d9b7
mugentsukoyomi-dot <ilhamputrapml@gmail.com>
2026-09-22 11:55:29 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,10 @@ app = FastAPI(title="Phishing Detector API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://phishing-detector-lake.vercel.app/",  # ganti sesuai domain Vercel asli kamu
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
